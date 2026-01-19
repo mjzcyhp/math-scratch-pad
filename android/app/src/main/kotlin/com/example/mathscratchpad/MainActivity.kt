a30d8c05eee05cc5dd03320bd7a2e538ab5ca236
mjzcyhp <532543942@qq.com>
2026-01-20 04:56:10 +0800

    Rename package and update MainActivity class

--- android/app/src/main/kotlin/com/example/mathscratchpad/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/mathscratchpad/MainActivity.kt
@@ -1,6 +1,6 @@
-package com.example.mathscratchpad
+package com.yourcompany.yourappname
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
 }
