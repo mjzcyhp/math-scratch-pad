e51f57d0b83b5c0815b72bf70f6ab07e0dc32c57
mjzcyhp <532543942@qq.com>
2026-01-20 03:07:30 +0800

    Refactor ScratchPage to use StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -16,25 +16,55 @@ class MathScratchApp extends StatelessWidget {
   }
 }
 
-class ScratchPage extends StatelessWidget {
+class ScratchPage extends StatefulWidget {
   const ScratchPage({super.key});
+
+  @override
+  State<ScratchPage> createState() => _ScratchPageState();
+}
+
+class _ScratchPageState extends State<ScratchPage> {
+  List<Offset?> points = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: GestureDetector(
-        onPanUpdate: (_) {},
-        child: const Center(
-          child: Text(
-            '空白草稿纸',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 18,
-            ),
-          ),
+        onPanUpdate: (details) {
+          setState(() {
+            RenderBox box = context.findRenderObject() as RenderBox;
+            points.add(box.globalToLocal(details.globalPosition));
+          });
+        },
+        onPanEnd: (_) => points.add(null),
+        child: CustomPaint(
+          painter: ScratchPainter(points),
+          size: Size.infinite,
         ),
       ),
     );
   }
+}
+
+class ScratchPainter extends CustomPainter {
+  final List<Offset?> points;
+  ScratchPainter(this.points);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..color = Colors.black
+      ..strokeWidth = 3.0
+      ..strokeCap = StrokeCap.round;
+
+    for (int i = 0; i < points.length - 1; i++) {
+      if (points[i] != null && points[i + 1] != null) {
+        canvas.drawLine(points[i]!, points[i + 1]!, paint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(ScratchPainter oldDelegate) => true;
 }
